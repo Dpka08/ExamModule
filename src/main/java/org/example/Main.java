81adb1ded21bb93431b61c04bc0a1157abc3ef83
deepika <dpgrg1997@gmail.com>
2024-03-17 21:07:50 -0400

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) throws IOException{
+        // read content from excel file -- binary object-- java object.
+        try{
+    File file = new File("/Users/dipeshgurung/eclipse-workspace/ExamModule/src/main/resources/employee.xlsx");
+    FileInputStream fileInputStream = new FileInputStream(file);
+
+    // convert binary to xssf work book object.
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+
+
+            // read from the column.
+            Iterable<column> columnIterable= row.columnIterable();
+            while(columnIterable.hasnext()){
+                column column = columnIterable.next();
+                System.out.println("column"+(column.getColumnIndex()+1));
+                System.out.println("Value:");
+                switch (column.getColumnType()){
+                    case STRING:
+                        System.out.println(column.getStringCellValue());
+                        break;
+                    case NUMERIC:
+                        System.out.println(column.getNumericColumnValue());
+                        break;
+                    case BOOLEAN:
+                        System.out.println(column.getBooleanValue());
+                        break;
+
+
+                }
+
+            }
+        }
+
+
+
+
+
+
 }
---
+++ src/main/java/org/example/ReadFromExcel.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFromExcel {
+    public static <XSSFSheet> void main(String[] args) throws IOException {
+        // read content from excel file -- binary object-- java object.
+        try{
+        File file = new File("/Users/dipeshgurung/eclipse-workspace/ExamModule/src/main/resources/employee.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // convert binary to xssf work book object.
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(file);
+            HSSFWorkbook workbook = null;
+            XSSFSheet sheet = workbook.getSheetAt((0);
+
+
+
+        // read from the column.
+        Iterable<column> columnIterable= row.columnIterable();
+        while(columnIterable.hasnext()){
+            column column = columnIterable.next();
+            System.out.println("column"+(column.getColumnIndex()+1));
+            System.out.println("Value:");
+            switch (column.getColumnType()){
+                case STRING:
+                    System.out.println(column.getStringCellValue());
+                    break;
+                case NUMERIC:
+                    System.out.println(column.getNumericColumnValue());
+                    break;
+                case BOOLEAN:
+                    System.out.println(column.getBooleanValue());
+                    break;
+
+
+            }
+
+        }
+    }
+
+
+
+
+
+
+}
